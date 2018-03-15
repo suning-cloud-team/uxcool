60c3c5245e4f27a9eb1ef24d2401cd0b833399b7
hw <17061765@cnsuning.com>
2018-03-15 10:48:21 +0800

    add cloneVNode

--- packages/v-utils/src/utils.js
+++ packages/v-utils/src/utils.js
@@ -1,3 +1,5 @@
+import isPlainObject from 'lodash/isPlainObject';
+
 export function isFunction(fn) {
   return typeof fn === 'function';
 }
@@ -6,6 +8,8 @@ export function isArray(arr) {
   return Array.isArray(arr);
 }
 
-export function isPlainObject(obj) {
-  return Object.prototype.toString.call(obj) === '[object Object]';
-}
+// export function isPlainObject(obj) {
+//   return Object.prototype.toString.call(obj) === '[object Object]';
+// }
+
+export { isPlainObject };
--- packages/v-utils/src/vnode.js
+++ packages/v-utils/src/vnode.js
@@ -1,10 +1,69 @@
+import pick from 'lodash/pick';
+import assign from 'lodash/assign';
 import { isFunction } from './utils';
+
+const DATA_KEYS = [
+  'class',
+  'staticClass',
+  'style',
+  'attrs',
+  'props',
+  'domProps',
+  'on',
+  'nativeOn',
+  'directives',
+  'scopesSlots',
+  'slot',
+  'ref',
+  'key',
+];
 
 export function isVNode(node = {}) {
   if (node === null || typeof node !== 'object' || !node.constructor) {
     return false;
   }
   return !!('componentOptions' in node && 'tag' in node && 'ns' in node);
+}
+
+function mutateKey(key) {
+  return `${key}-cloned-cid`;
+}
+
+function extractData(vnode, isComp) {
+  const data = pick(vnode.data, DATA_KEYS);
+  if (isComp) {
+    const cOpts = vnode.componentOptions;
+    assign(data, {
+      props: cOpts.propsData,
+      on: cOpts.listeners,
+    });
+  }
+
+  if (data.key) {
+    data.key = mutateKey(data.key);
+  }
+
+  return data;
+}
+
+export function cloneVNode(vnode, newData = {}) {
+  if (!isVNode(vnode)) {
+    return vnode;
+  }
+  // use the context that the original vnode was created in.
+  const h = vnode.context && vnode.context.$createElement;
+  const isComp = !!vnode.componentOptions;
+  const isText = !vnode.tag; // this will also match comments but those will be dropped, essentially
+  const children = isComp ? vnode.componentOptions.children : vnode.children;
+
+  if (isText) return vnode.text;
+
+  const data = extractData(vnode, isComp);
+
+  const tag = isComp ? vnode.componentOptions.Ctor : vnode.tag;
+
+  const childNodes = children ? children.map(c => cloneVNode(c)) : undefined;
+  return h(tag, data, childNodes);
 }
 
 export function getVNodeOptions(node) {
