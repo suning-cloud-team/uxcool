dbf698d49db3a134593d035c2a4e231598e9f4a5
hw <17061765@cnsuning.com>
2018-03-15 10:49:07 +0800

    move isVNode, cloneVNode to v-utils

--- packages/v-table/src/utils.js
+++ packages/v-table/src/utils.js
@@ -140,13 +140,6 @@ export function groupRows(columns = []) {
   return recursiveRow(columns, 0, []);
 }
 
-export function isVNode(node = {}) {
-  if (node === null || typeof node !== 'object' || !node.constructor) {
-    return false;
-  }
-  return !!('componentOptions' in node && 'tag' in node && 'ns' in node);
-}
-
 const scrollBarStyle = {
   position: 'absolute',
   left: '-50px',
@@ -217,59 +210,4 @@ export function getRowStyle(rowId, rowsHeight) {
 
 export function getNormalizeContent(h, columnFixed, tableFixed, content) {
   return columnFixed && !tableFixed ? <span>&nbsp;</span> : content;
-}
-
-
-const DATA_KEYS = [
-  'class',
-  'staticClass',
-  'style',
-  'attrs',
-  'props',
-  'domProps',
-  'on',
-  'nativeOn',
-  'directives',
-  'scopesSlots',
-  'slot',
-  'ref',
-  'key',
-];
-
-function mutateKey(key) {
-  return `${key}-cloned-cid`;
-}
-
-function extractData(vnode, isComp) {
-  const data = _.pick(vnode.data, DATA_KEYS);
-  if (isComp) {
-    const cOpts = vnode.componentOptions;
-    _.assign(data, {
-      props: cOpts.propsData,
-      on: cOpts.listeners,
-    });
-  }
-
-  if (data.key) {
-    data.key = mutateKey(data.key);
-  }
-
-  return data;
-}
-
-export function cloneVNode(vnode, newData = {}) {
-  // use the context that the original vnode was created in.
-  const h = vnode.context && vnode.context.$createElement;
-  const isComp = !!vnode.componentOptions;
-  const isText = !vnode.tag; // this will also match comments but those will be dropped, essentially
-  const children = isComp ? vnode.componentOptions.children : vnode.children;
-
-  if (isText) return vnode.text;
-
-  const data = extractData(vnode, isComp);
-
-  const tag = isComp ? vnode.componentOptions.Ctor : vnode.tag;
-
-  const childNodes = children ? children.map(c => cloneVNode(c)) : undefined;
-  return h(tag, data, childNodes);
 }
