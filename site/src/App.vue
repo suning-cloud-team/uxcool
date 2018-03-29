17dbc3ed87138fa751d83f98e50b0a2013f2ecd4
RoyXun <963877037@qq.com>
2018-03-29 10:41:13 +0800

    修改主要样式 有部分组件的深色主题不好从网站修改

--- site/src/App.vue
+++ site/src/App.vue
@@ -1,11 +1,12 @@
 <template>
-    <div id="app">
-        <router-view></router-view>
-    </div>
+  <div id="app"
+       class="uxcool">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'App',
-};
+  export default {
+    name: 'App',
+  };
 </script>
