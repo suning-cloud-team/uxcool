6fdbf0c4f4117b616a28a7b00e2d7e0cd0a2ab94
RoyXun <963877037@qq.com>
2018-05-18 11:38:39 +0800

    add input-number anchor divider notification components

---
+++ site/src/views/components/anchor/index.vue
@@ -0,0 +1,87 @@
+<template>
+  <article class="doc">
+    <h1 class="ux-heading">Anchor
+      <span class="subtitle">锚点</span>
+    </h1>
+
+    <ux-heading level="2"
+                id="overview">概述</ux-heading>
+    <p>聚合信息的一种容器，通常包含标题、内容和操作区域。</p>
+    <!-- <demo-basic></demo-basic>
+    <demo-flat></demo-flat>
+    <demo-vertical></demo-vertical> -->
+
+    <hr>
+    <ux-heading level="2"
+                id="api">API</ux-heading>
+    <table class="api-table">
+      <caption>Props</caption>
+      <thead>
+        <tr>
+          <th>属性</th>
+          <th>描述</th>
+          <th>类型</th>
+          <th>默认值</th>
+        </tr>
+      </thead>
+      <!-- <tbody>
+        <tr>
+          <td>flat</td>
+          <td>是否是纯色卡片</td>
+          <td>Boolean</td>
+          <td>false</td>
+        </tr>
+        <tr>
+          <td>vertical</td>
+          <td>是否是竖直卡片</td>
+          <td>Boolean</td>
+          <td>false</td>
+        </tr>
+        <tr>
+          <td>bodyStyle</td>
+          <td>卡片内容区样式</td>
+          <td>Object</td>
+          <td>-</td>
+        </tr>
+      </tbody> -->
+    </table>
+    <!-- <table class="api-table">
+      <caption>Slots</caption>
+      <thead>
+        <tr>
+          <th>名称</th>
+          <th>描述</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>title</td>
+          <td>卡片标题</td>
+        </tr>
+        <tr>
+          <td>action</td>
+          <td>操作，通常放一个
+            <code>a</code>标签包裹的字体图标。</td>
+        </tr>
+        <tr>
+          <td>无</td>
+          <td>卡片内容</td>
+        </tr>
+      </tbody>
+    </table> -->
+  </article>
+</template>
+
+<script>
+  // import demoBasic from './basic.vue';
+  // import demoFlat from './flat.vue';
+  // import demoVertical from './vertical.vue';
+
+  export default {
+    components: {
+      // demoBasic,
+      // demoFlat,
+      // demoVertical,
+    },
+  };
+</script>
---
+++ site/src/views/components/input-number/step.vue
@@ -0,0 +1,50 @@
+<template>
+  <ux-demo title="步长"
+           :height="200">
+    <div slot="demo">
+      <ux-input-number novalidate
+                       :value="num1"
+                       :step="0.1"
+                       :min="min"
+                       :max="max"
+                       name="abc"
+                       @change="onChange" /> {{ num1 }}
+      <ux-input-number novalidate
+                       v-model="num2"
+                       :step="0.1"
+                       :min="min"
+                       :max="max"
+                       name="abc"
+                       @change="onChange" /> {{ num2 }}
+    </div>
+    <div slot="desc">
+      可以通过
+      <code>step</code> 指定步长。
+    </div>
+    <ux-code slot="code">
+      {{ code }}
+    </ux-code>
+  </ux-demo>
+</template>
+
+<script>
+  import code from '@/code/input-number/basic.vue';
+
+  export default {
+    data() {
+      return {
+        code,
+        num1: 1,
+        num2: 0,
+        min: 1,
+        max: 10,
+      };
+    },
+    methods: {
+      onChange(val) {
+        console.log('changed', val);
+      },
+    },
+  };
+</script>
+
