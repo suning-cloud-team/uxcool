2b2f49bfc18c749caa99f8c226739582db65e38e
hw <17061765@cnsuning.com>
2018-07-16 17:55:28 +0800

    fix(Menu): 解决如果存在选中值, 当Menu子菜单数据发生变化时报错的问题
    Closes: #41

--- examples/views/menu/basic.vue
+++ examples/views/menu/basic.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="demo">
+    <!-- <div class="demo">
       <ux-menu mode="horizontal"
                theme="dark">
         <ux-menu-item name="item-1">
@@ -48,103 +48,102 @@
       </ux-menu>
     </div>
 
-    <!--vertical -->
     <div class="demo">
-      <ux-menu style="width:200px;">
-        <ux-menu-item name="item-1">
-          item-1
+    <ux-menu style="width:200px;">
+      <ux-menu-item name="item-1">
+        item-1
+      </ux-menu-item>
+      <ux-menu-item name="item-2">
+        item-2
+      </ux-menu-item>
+      <ux-sub-menu name="sub-menu-1"
+                   title="sub-menu-1">
+        <ux-menu-item name="sub-menu-1-1">sub-menu-1-1</ux-menu-item>
+        <ux-menu-item name="sub-menu-1-2">sub-menu-1-2</ux-menu-item>
+      </ux-sub-menu>
+      <ux-sub-menu name="sub-menu-2"
+                   title="sub-menu-2">
+        <ux-menu-item disabled
+                      name="sub-menu-2-1">sub-menu-1-1</ux-menu-item>
+        <ux-sub-menu title="sub-menu-2-2"
+                     name="sub-menu-2-2">
+          <ux-menu-item name="sub-menu-2-2-1">sub-menu-1-1</ux-menu-item>
+          <ux-menu-item name="sub-menu-2-2-2">sub-menu-1-2</ux-menu-item>
+        </ux-sub-menu>
+      </ux-sub-menu>
+      <ux-sub-menu name="sub-menu-3"
+                   title="sub-menu-3">
+        <ux-menu-item-group title="测试-group">
+          <ux-menu-item name="sub-menu-3-1-1">sub-menu-3-1-1</ux-menu-item>
+        </ux-menu-item-group>
+        <ux-menu-item-group title="测试-group">
+          <ux-menu-item name="sub-menu-3-2-1">sub-menu-3-2-1</ux-menu-item>
+        </ux-menu-item-group>
+        <ux-menu-item name="sub-menu-3-3"
+                      title="sub-menu-3-3">
+          sub-menu-3-3
         </ux-menu-item>
-        <ux-menu-item name="item-2">
-          item-2
-        </ux-menu-item>
-        <ux-sub-menu name="sub-menu-1"
-                     title="sub-menu-1">
-          <ux-menu-item name="sub-menu-1-1">sub-menu-1-1</ux-menu-item>
-          <ux-menu-item name="sub-menu-1-2">sub-menu-1-2</ux-menu-item>
+        <ux-sub-menu name="sub-menu-3-4"
+                     title="sub-menu-3-4">
+          <ux-menu-item name="sub-menu-3-4-1">sub-menu-3-4-1</ux-menu-item>
+          <ux-menu-item name="sub-menu-3-4-2">sub-menu-3-4-2</ux-menu-item>
         </ux-sub-menu>
-        <ux-sub-menu name="sub-menu-2"
-                     title="sub-menu-2">
-          <ux-menu-item disabled
-                        name="sub-menu-2-1">sub-menu-1-1</ux-menu-item>
-          <ux-sub-menu title="sub-menu-2-2"
-                       name="sub-menu-2-2">
-            <ux-menu-item name="sub-menu-2-2-1">sub-menu-1-1</ux-menu-item>
-            <ux-menu-item name="sub-menu-2-2-2">sub-menu-1-2</ux-menu-item>
-          </ux-sub-menu>
-        </ux-sub-menu>
-        <ux-sub-menu name="sub-menu-3"
-                     title="sub-menu-3">
-          <ux-menu-item-group title="测试-group">
-            <ux-menu-item name="sub-menu-3-1-1">sub-menu-3-1-1</ux-menu-item>
-          </ux-menu-item-group>
-          <ux-menu-item-group title="测试-group">
-            <ux-menu-item name="sub-menu-3-2-1">sub-menu-3-2-1</ux-menu-item>
-          </ux-menu-item-group>
-          <ux-menu-item name="sub-menu-3-3"
-                        title="sub-menu-3-3">
-            sub-menu-3-3
-          </ux-menu-item>
-          <ux-sub-menu name="sub-menu-3-4"
-                       title="sub-menu-3-4">
-            <ux-menu-item name="sub-menu-3-4-1">sub-menu-3-4-1</ux-menu-item>
-            <ux-menu-item name="sub-menu-3-4-2">sub-menu-3-4-2</ux-menu-item>
-          </ux-sub-menu>
-        </ux-sub-menu>
-      </ux-menu>
-    </div>
+      </ux-sub-menu>
+    </ux-menu>
+  </div>
 
-    <!--vertical -->
-    <div class="demo">
-      <ux-menu theme="dark"
-               style="width:200px;">
-        <ux-menu-item name="item-1">
-          item-1
+  <div class="demo">
+    <ux-menu theme="dark"
+             style="width:200px;">
+      <ux-menu-item name="item-1">
+        item-1
+      </ux-menu-item>
+      <ux-menu-item name="item-2">
+        item-2
+      </ux-menu-item>
+      <ux-sub-menu name="sub-menu-1"
+                   title="sub-menu-1">
+        <ux-menu-item name="sub-menu-1-1">sub-menu-1-1</ux-menu-item>
+        <ux-menu-item name="sub-menu-1-2">sub-menu-1-2</ux-menu-item>
+      </ux-sub-menu>
+      <ux-sub-menu name="sub-menu-2"
+                   title="sub-menu-2">
+        <ux-menu-item disabled
+                      name="sub-menu-2-1">sub-menu-1-1</ux-menu-item>
+        <ux-sub-menu title="sub-menu-2-2"
+                     name="sub-menu-2-2">
+          <ux-menu-item name="sub-menu-2-2-1">sub-menu-1-1</ux-menu-item>
+          <ux-menu-item name="sub-menu-2-2-2">sub-menu-1-2</ux-menu-item>
+        </ux-sub-menu>
+      </ux-sub-menu>
+      <ux-sub-menu name="sub-menu-3"
+                   title="sub-menu-3">
+        <ux-menu-item-group title="测试-group">
+          <ux-menu-item name="sub-menu-3-1-1">sub-menu-3-1-1</ux-menu-item>
+        </ux-menu-item-group>
+        <ux-menu-item-group title="测试-group">
+          <ux-menu-item name="sub-menu-3-2-1">sub-menu-3-2-1</ux-menu-item>
+        </ux-menu-item-group>
+        <ux-menu-item name="sub-menu-3-3"
+                      title="sub-menu-3-3">
+          sub-menu-3-3
         </ux-menu-item>
-        <ux-menu-item name="item-2">
-          item-2
-        </ux-menu-item>
-        <ux-sub-menu name="sub-menu-1"
-                     title="sub-menu-1">
-          <ux-menu-item name="sub-menu-1-1">sub-menu-1-1</ux-menu-item>
-          <ux-menu-item name="sub-menu-1-2">sub-menu-1-2</ux-menu-item>
+        <ux-sub-menu name="sub-menu-3-4"
+                     title="sub-menu-3-4">
+          <ux-menu-item name="sub-menu-3-4-1">sub-menu-3-4-1</ux-menu-item>
+          <ux-menu-item name="sub-menu-3-4-2">sub-menu-3-4-2</ux-menu-item>
         </ux-sub-menu>
-        <ux-sub-menu name="sub-menu-2"
-                     title="sub-menu-2">
-          <ux-menu-item disabled
-                        name="sub-menu-2-1">sub-menu-1-1</ux-menu-item>
-          <ux-sub-menu title="sub-menu-2-2"
-                       name="sub-menu-2-2">
-            <ux-menu-item name="sub-menu-2-2-1">sub-menu-1-1</ux-menu-item>
-            <ux-menu-item name="sub-menu-2-2-2">sub-menu-1-2</ux-menu-item>
-          </ux-sub-menu>
-        </ux-sub-menu>
-        <ux-sub-menu name="sub-menu-3"
-                     title="sub-menu-3">
-          <ux-menu-item-group title="测试-group">
-            <ux-menu-item name="sub-menu-3-1-1">sub-menu-3-1-1</ux-menu-item>
-          </ux-menu-item-group>
-          <ux-menu-item-group title="测试-group">
-            <ux-menu-item name="sub-menu-3-2-1">sub-menu-3-2-1</ux-menu-item>
-          </ux-menu-item-group>
-          <ux-menu-item name="sub-menu-3-3"
-                        title="sub-menu-3-3">
-            sub-menu-3-3
-          </ux-menu-item>
-          <ux-sub-menu name="sub-menu-3-4"
-                       title="sub-menu-3-4">
-            <ux-menu-item name="sub-menu-3-4-1">sub-menu-3-4-1</ux-menu-item>
-            <ux-menu-item name="sub-menu-3-4-2">sub-menu-3-4-2</ux-menu-item>
-          </ux-sub-menu>
-        </ux-sub-menu>
-      </ux-menu>
-    </div>
+      </ux-sub-menu>
+    </ux-menu>
+  </div>-->
+
+    <selected-demo />
   </div>
 </template>
 
 <script>
-  // import '@suning/uxcool/src/components/menu/style/index.scss';
-  // import { UxMenu, UxMenuItem, UxMenuItemGroup, UxSubMenu } from '@suning/uxcool/src/components/menu';
   import { UxMenu } from '@suning/uxcool';
+  import SelectedDemo from './selected.vue';
 
   const { SubMenu: UxSubMenu, MenuItemGroup: UxMenuItemGroup, MenuItem: UxMenuItem } = UxMenu;
   export default {
@@ -153,6 +152,7 @@
       UxSubMenu,
       UxMenuItemGroup,
       UxMenuItem,
+      SelectedDemo,
     },
   };
 </script>
