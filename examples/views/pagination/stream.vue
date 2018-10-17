ff4c1e56cd386e370b2c7c5b34070e246aabd4e7
hw <17061765@cnsuning.com>
2018-10-17 20:53:39 +0800

    feat(Pagination): 新增 stream mode,支持 类似 baidu 搜索分页模式
    Closes #98

---
+++ examples/views/pagination/stream.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="demo">
+    <h4>Stream</h4>
+    <ux-pagination :total="total"
+                   mode="stream" />
+  </div>
+</template>
+
+
+<script>
+  import { Pagination } from '@suning/uxcool';
+
+  export default {
+    components: {
+      UxPagination: Pagination,
+    },
+    data() {
+      return {
+        total: 100,
+      };
+    },
+    created() {
+      setTimeout(() => {
+        this.total = 250;
+      }, 1500);
+    },
+  };
+</script>
