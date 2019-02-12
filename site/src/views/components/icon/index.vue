8db86177ab0fda5307f07d29df0fe285cfdcd6d9
RoyXun <963877037@qq.com>
2019-02-12 10:07:05 +0800

    feat(icon):修改字体图标及命名

--- site/src/views/components/icon/index.vue
+++ site/src/views/components/icon/index.vue
@@ -3,6 +3,28 @@
     <h1 class="ux-heading">Icon
       <span class="subtitle">图标</span>
     </h1>
+    <h2 class="ux-heading">命名规则</h2>
+    <p>图标命名以后缀区分类型：</p>
+    <ul>
+      <li>
+        <p><code>-d</code> 常用图标</p>
+      </li>
+      <li>
+        <p><code>-a</code> 箭头图标</p>
+      </li>
+      <li>
+        <p><code>-t</code> 提示性图标 </p>
+      </li>
+      <li>
+        <p><code>-m</code> 实体图标</p>
+      </li>
+      <li>
+        <p><code>-b</code> 粗体图标</p>
+      </li>
+    </ul>
+    <blockquote>
+      <p><code>0.5.0-next.36</code>版本对图标进行了重命名, 增删了部分图标。为了向后兼容，大部分旧图标的旧命名还会保留一段时间(不推荐使用)，但可能会在后续版本中移除。建议有条件的项目组使用最新的图标命名。</p>
+    </blockquote>
     <h2 class="ux-heading">
       如何使用
     </h2>
@@ -11,34 +33,88 @@
     </ux-code>
     <h2 class="ux-heading">图标列表</h2>
 
-    <ux-row flex>
-      <ux-col v-clipboard="copyText(icon)"
-              v-for="(icon, i) in list"
-              :xl="4"
-              :lg="6"
-              :md="8"
-              :sm="12"
-              :key="i"
-              class="icon-item"
-              @success="onCopySuccess">
-        <ux-icon :type="icon.name" />
-        <div class="icon-item-code">{{ icon.font }}</div>
-        <div class="icon-item-name">{{ icon.name }}</div>
-      </ux-col>
-    </ux-row>
+    <ux-tabs>
+      <ux-tab-pane v-for="(group, index) in list"
+                   :tab="group.name"
+                   :name="group.name"
+                   :key="index">
+        <div class="p-xs-2">
+          <ux-row flex>
+            <ux-col v-clipboard="copyText(icon)"
+                    v-for="(icon, i) in group.icons"
+                    :xl="4"
+                    :lg="6"
+                    :md="8"
+                    :sm="12"
+                    :key="i"
+                    class="icon-item"
+                    @success="onCopySuccess">
+              <ux-icon :type="icon.name" />
+              <div class="icon-item-code">{{ icon.font }}</div>
+              <div class="icon-item-name">{{ icon.name }}</div>
+            </ux-col>
+          </ux-row>
+
+        </div>
+      </ux-tab-pane>
+    </ux-tabs>
   </div>
 </template>
 
 <script>
   import UxMessage from '@suning/uxcool/es/message';
-  import list from './icon';
+  import icon from './icon';
 
   export default {
     data() {
       return {
-        list,
         code: '<ux-icon type="down" />',
+        list: [],
       };
+    },
+    created() {
+      const arrows = {
+        name: '箭头',
+        icons: [],
+      };
+      const tips = {
+        name: '提示',
+        icons: [],
+      };
+      const faces = {
+        name: '实体',
+        icons: [],
+      };
+      const bold = {
+        name: '粗线条',
+        icons: [],
+      };
+      const general = {
+        name: '常用',
+        icons: [],
+      };
+
+      const REG_ARROW = /-a$/;
+      const REG_TIP = /-t$/;
+      const REG_FACE = /-m$/;
+      const REG_BOLD = /-b$/;
+      const REG_GENERAL = /-d$/;
+      icon.forEach((item) => {
+        const { name } = item;
+        if (REG_ARROW.test(name)) {
+          arrows.icons.push(item);
+        } else if (REG_TIP.test(name)) {
+          tips.icons.push(item);
+        } else if (REG_FACE.test(name)) {
+          faces.icons.push(item);
+        } else if (REG_BOLD.test(name)) {
+          bold.icons.push(item);
+        } else if (REG_GENERAL.test(name)) {
+          general.icons.push(item);
+        }
+      });
+
+      this.list = [general, arrows, tips, faces, bold];
     },
     methods: {
       copyText({ name }) {
@@ -54,6 +130,7 @@
 <style lang="scss" scoped>
   .icon-item {
     padding: 16px;
+    color: #666;
     text-align: center;
     cursor: pointer;
 
