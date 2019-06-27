f8d3b7004dcdbe0d9a32dc14dc52873d6c37529a
蒋弼政 <19040734@cnsuning.com>
2019-06-27 17:20:54 +0800

    fix(anchor): 修复了ux-anchor的问题
    Closes #164

--- examples/views/anchor/basic.vue
+++ examples/views/anchor/basic.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <normal-demo />
-    <offset-demo />
+    <!-- <offset-demo /> -->
     <!-- <container-demo /> -->
 
   </div>
---
+++ packages/uxcool/src/components/anchor/anchorLink.vue
@@ -0,0 +1,65 @@
+<template>
+  <div :class="classes">
+    <a :class="titleClasses"
+       :href="href"
+       @click.prevent="onClickAnchor"
+    >
+      {{ title }}
+    </a>
+    <slot />
+  </div>
+</template>
+
+<script>
+  import { buildComponentName } from '../utils';
+
+  export default {
+    name: buildComponentName('AnchorLink'),
+    inject: ['anchor'],
+    props: {
+      href: {
+        type: String,
+        required: true,
+      },
+      title: {
+        type: String,
+        required: true,
+      }
+    },
+    created() {
+      this.anchor.titles.push(this.href);
+    },
+    computed: {
+      prefixCls() {
+        return `${this.anchor.prefixCls}-link`;
+      },
+      isActive() {
+        return this.anchor.currentLink === this.href;
+      },
+      classes() {
+        const { prefixCls, isActive } = this;
+        return {
+          [prefixCls]: true,
+          [`${prefixCls}-active`]: isActive,
+        };
+      },
+      titleClasses() {
+        const { prefixCls, isActive } = this;
+        return {
+          [`${prefixCls}-title`]: true,
+          [`${prefixCls}-title-active`]: isActive,
+        };
+      },
+    },
+    methods: {
+      onClickAnchor() {
+        const isRoute = this.$router;
+        if (isRoute) {
+          this.$router.push(this.href);
+        } else {
+          window.location.href = this.href;
+        }
+      },
+    },
+  };
+</script>
