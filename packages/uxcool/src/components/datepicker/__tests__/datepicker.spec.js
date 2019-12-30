358dc5399b3e1f599422498ed14923b53f53cdc7
前端-何伟(17061765) <17061765@cnsuning.com>
2019-12-30 10:18:30 +0800

    test(DatePicker): 新增`DatePicker`单元测试

---
+++ packages/uxcool/src/components/datepicker/__tests__/datepicker.spec.js
@@ -0,0 +1,220 @@
+import { mount, waitTime } from '@suning/v-test-utils';
+import {
+  format as formatDate, subDays, subMonths, addMonths, subYears, addDays
+} from 'date-fns';
+import { selectDate, getPortal, triggerEvent } from './utils';
+import DatePicker from '../datepicker.vue';
+
+describe('DatePicker', () => {
+  it('create', () => {
+    const format = 'YYYY-MM-DD';
+    const placeholder = 'create-test';
+    const id = 'test-a';
+    const wrapper = mount(DatePicker, {
+      propsData: {
+        format,
+        placeholder,
+      },
+      attrs: {
+        id,
+      },
+    });
+
+    const input = wrapper.find('input');
+
+    expect(input.attributes('placeholder')).toBe(placeholder);
+    expect(wrapper.element.id).toBe(id);
+    // const selectedDate = formatDate(subDays(new Date(), 1), format);
+    // const input = wrapper.find('input');
+    // input.trigger('click');
+
+    // await waitTime(20);
+    // const triggerWrapper = wrapper.find(Trigger);
+    // const portalWrapper = createWrapper(triggerWrapper.vm.portal.$el);
+    // console.log('portalWrapper, %o', portalWrapper);
+    // const a = portalWrapper.find(`[role="gridcell"][title="${selectedDate}"]`);
+    // console.log('a%o', a);
+    // portalWrapper.find(`[role="gridcell"][title="${selectedDate}"]`).trigger('click');
+
+    // expect(wrapper.find('input').text()).toBe(selectedDate);
+  });
+
+  it('select date', async () => {
+    const format = 'YYYY-MM-DD';
+    const wrapper = mount(DatePicker, {
+      propsData: {
+        format,
+        // isOpen: true,
+      },
+    });
+    // wait render
+    await waitTime(20);
+
+    const inputWrapper = wrapper.find('.ux-input');
+    await triggerEvent(wrapper, 'click');
+    const portalWrapper = await getPortal(wrapper);
+
+    const prevDate = formatDate(subDays(new Date(), 1), format);
+    await selectDate(portalWrapper, prevDate);
+    expect(inputWrapper.element.value).toBe(prevDate);
+
+    const prevMonth = formatDate(subMonths(new Date(), 1), format);
+    await selectDate(portalWrapper, prevMonth);
+    expect(inputWrapper.element.value).toBe(prevMonth);
+  });
+
+  it('is-open', async () => {
+    const wrapper = mount(DatePicker, {
+      propsData: {
+        isOpen: true,
+      },
+    });
+    await waitTime(20);
+
+    const portal = await getPortal(wrapper);
+    expect(portal.find('.ux-calendar-picker-container').exists()).toBeTruthy();
+
+    wrapper.setProps({
+      isOpen: false,
+    });
+    // animate delay
+    await waitTime(300);
+    expect(portal.find('.ux-calendar-picker-container').exists()).toBeFalsy();
+
+    wrapper.setProps({
+      isOpen: true,
+    });
+    // animate delay
+    await waitTime(300);
+    expect(portal.find('.ux-calendar-picker-container').exists()).toBeTruthy();
+  });
+
+  it('value(v-model) no transitionName', async () => {
+    const format = 'YYYY-MM-DD';
+    const Test = {
+      data() {
+        return {
+          value: null,
+        };
+      },
+      methods: {
+        onInput(value) {
+          this.value = value;
+        },
+      },
+      render() {
+        const { value, onInput } = this;
+        return <DatePicker value={value} transitionName="" on-input={onInput} />;
+      },
+    };
+
+    const wrapper = mount(Test);
+    const input = wrapper.find('.ux-input');
+    await waitTime(20);
+    const dp = wrapper.find(DatePicker);
+    await triggerEvent(dp, 'click');
+    const portal = await getPortal(wrapper);
+
+    await triggerEvent(portal.find('.ux-calendar-next-month-btn'), 'click');
+    const nextMonth = formatDate(addMonths(new Date(), 1), format);
+    await selectDate(portal, nextMonth);
+    expect(formatDate(wrapper.vm.value, format)).toBe(nextMonth);
+    expect(input.element.value).toBe(nextMonth);
+
+    await triggerEvent(dp, 'click');
+    const prevMonthBtn = portal.find('.ux-calendar-prev-month-btn');
+    await triggerEvent(prevMonthBtn, 'click');
+    await triggerEvent(prevMonthBtn, 'click');
+    const prevMonth = formatDate(subMonths(new Date(), 1), format);
+    await selectDate(portal, prevMonth);
+    expect(formatDate(wrapper.vm.value, format)).toBe(prevMonth);
+    expect(input.element.value).toBe(prevMonth);
+
+    await triggerEvent(dp, 'click');
+
+    const nDate = subYears(new Date(), 1);
+    wrapper.setData({
+      value: nDate,
+    });
+    await waitTime(20);
+
+    const selectedElement = portal.find('.ux-calendar-selected-date');
+    expect(selectedElement.exists()).toBeTruthy();
+
+    expect(selectedElement.attributes('title')).toBe(formatDate(nDate, format));
+  });
+
+  it('disabled', async () => {
+    const wrapper = mount(DatePicker, {
+      propsData: {
+        disabled: true,
+      },
+    });
+    await waitTime(20);
+    expect(wrapper.find('.ux-input-disabled').exists()).toBeTruthy();
+
+    wrapper.setProps({
+      disabled: false,
+    });
+    await waitTime(20);
+    expect(wrapper.find('.ux-input-disabled').exists()).toBeFalsy();
+  });
+
+  it('format DD/MM/YYYY', async () => {
+    const format = 'DD/MM/YYYY';
+    const today = new Date();
+    const wrapper = mount(DatePicker, {
+      propsData: {
+        value: today,
+        format,
+        transitionName: '',
+      },
+    });
+
+    await waitTime(20);
+
+    const input = wrapper.find('.ux-input');
+    expect(input.element.value).toBe(formatDate(today, format));
+
+    await triggerEvent(wrapper, 'click');
+
+    const portal = await getPortal(wrapper);
+    const nDate = formatDate(addDays(new Date(), 2), format);
+    await selectDate(portal, nDate);
+    expect(input.element.value).toBe(nDate);
+  });
+
+  it.only('show-time', async () => {
+    jest.setTimeout(50000);
+    const wrapper = mount(DatePicker, {
+      propsData: {
+        showTime: true,
+        transitionName: '',
+      },
+    });
+
+    await waitTime(20);
+
+    await triggerEvent(wrapper, 'click');
+
+    const portal = await getPortal(wrapper);
+
+    const timePickerBtn = portal.find('.ux-calendar-time-picker-btn');
+    const okBtn = portal.find('.ux-calendar-ok-btn');
+    expect(timePickerBtn.exists()).toBeTruthy();
+    expect(okBtn.exists()).toBeTruthy();
+
+    await triggerEvent(timePickerBtn, 'click');
+    const timerPicker = portal.find('.ux-calendar-time-picker');
+    expect(timerPicker.exists()).toBeTruthy();
+
+    wrapper.setProps({
+      showTime: false,
+    });
+    await waitTime(20);
+
+    expect(timePickerBtn.exists()).toBeFalsy();
+    expect(okBtn.exists()).toBeFalsy();
+    expect(timerPicker.exists()).toBeFalsy();
+  });
+});
---
+++ packages/uxcool/src/components/datepicker/__tests__/utils.js
@@ -0,0 +1,23 @@
+import { createWrapper, waitTime } from '@suning/v-test-utils';
+import { format as formatDate, isDate } from 'date-fns';
+import Trigger from '@suning/v-trigger';
+
+export async function selectDate(wrapper, date, format = 'YYYY-MM-DD', delay = 20) {
+  wrapper
+    .find(`[role="gridcell"][title="${isDate(date) ? formatDate(date, format) : date}"]`)
+    .trigger('click');
+
+  await waitTime(delay);
+}
+
+export function noop() {}
+export async function getPortal(wrapper) {
+  const trigger = wrapper.find(Trigger);
+  const portal = createWrapper(trigger.vm.portal.$el);
+  return portal;
+}
+
+export async function triggerEvent(wrapper, eventName, delay = 20) {
+  wrapper.trigger(eventName);
+  await waitTime(delay);
+}
