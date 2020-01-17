97a5af9fc4e877ba551f34c2ab83f96e3935c8af
前端-黄伟伟(19092346) <19092346@suning.com>
2020-01-17 11:58:46 +0800

    test(slider): 完善测试用例

--- packages/uxcool/src/components/slider/__tests__/slider.spec.js
+++ packages/uxcool/src/components/slider/__tests__/slider.spec.js
@@ -1,4 +1,4 @@
-import { mountPickerFactory, waitTime } from '@suning/v-test-utils';
+import { mountPickerFactory, waitTime, $ } from '@suning/v-test-utils';
 import Slider from '..';
 
 describe('Slider', () => {
@@ -7,6 +7,7 @@ describe('Slider', () => {
     const wrapper = await mountSlider({
       propsData: {
         min: 30,
+        value: [10, 40]
       },
     });
 
@@ -22,5 +23,71 @@ describe('Slider', () => {
     wrapper.setProps({ value: 90 });
     await waitTime();
     expect(wrapper.find('.ux-slider-handle').attributes('aria-valuenow')).toBe('80');
+  });
+
+  it('mouse event is correctly', async () => {
+    const wrapper = await mountSlider({
+      propsData: {
+        value: 80,
+      },
+    });
+    wrapper.find('.ux-slider-handle').trigger('mouseenter');
+    await waitTime();
+    expect($('.ux-tooltip-inner').text()).toContain('80');
+    expect(wrapper.find('.ux-slider-handle').attributes('aria-valuenow')).toBe('80');
+    wrapper.find('.ux-slider-handle').trigger('mouseleave');
+  });
+
+  it('range and value is array correctly', async () => {
+    const wrapper = await mountSlider({
+      propsData: {
+        value: [20, 80],
+        range: true,
+        tabindex: [1, 2],
+      },
+    });
+    await waitTime();
+    expect(wrapper.find('.ux-slider-handle-1').attributes('aria-valuenow')).toBe('20');
+    expect(wrapper.find('.ux-slider-handle-1').attributes('tabindex')).toBe('1');
+    expect(wrapper.find('.ux-slider-handle-2').attributes('aria-valuenow')).toBe('80');
+    expect(wrapper.find('.ux-slider-handle-2').attributes('tabindex')).toBe('2');
+  });
+  it('range and value is number correctly', async () => {
+    const wrapper = await mountSlider({
+      propsData: {
+        value: 20,
+        range: true,
+      },
+    });
+    await waitTime();
+    expect(wrapper.find('.ux-slider-handle-1').attributes('aria-valuenow')).toBe('20');
+    expect(wrapper.find('.ux-slider-handle-2').attributes('aria-valuenow')).toBe('20');
+  });
+
+  it('range and value is not defined  is correctly', async () => {
+    const wrapper = await mountSlider({
+      propsData: {
+        range: true
+      },
+    });
+    await waitTime();
+    expect(wrapper.find('.ux-slider-handle-1').attributes('aria-valuenow')).toBe('0');
+    expect(wrapper.find('.ux-slider-handle-2').attributes('aria-valuenow')).toBe('0');
+  });
+
+  it('tipFormat correct', async () => {
+    const tipFormat = jest.fn((v) => `${v}%`);
+    const wrapper = await mountSlider({
+      propsData: {
+        value: 20,
+        tabindex: [1, 2],
+        tipFormatter: tipFormat
+      },
+    });
+    wrapper.find('.ux-slider-handle').trigger('mouseenter');
+    await waitTime();
+    expect(tipFormat).toHaveBeenCalled();
+    expect($('.ux-tooltip-inner').text()).toContain('20%');
+    expect(wrapper.find('.ux-slider-handle').attributes('tabindex')).toBe('1');
   });
 });
