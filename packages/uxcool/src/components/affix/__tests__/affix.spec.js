23a1d7619c322ef8c039d85409807a61b726749d
前端-黄伟伟(19092346) <19092346@suning.com>
2020-01-22 18:31:06 +0800

    test(affix): 提高测试覆盖率

--- packages/uxcool/src/components/affix/__tests__/affix.spec.js
+++ packages/uxcool/src/components/affix/__tests__/affix.spec.js
@@ -22,4 +22,67 @@ it('offset top render correctly', async () => {
   affixWrapper.vm.updatePosition();
   await waitTime(200);
   expect(affixWrapper.element.style.height).toBe('0px');
+  affixWrapper.destroy();
+});
+
+it('offset bottom render correctly', async () => {
+  Element.prototype.getBoundingClientRect = jest.fn(() => ({
+    top: 203,
+    left: 240,
+    bottom: 353,
+    right: 1309,
+    width: 1069,
+    height: 150,
+    cw: 1050,
+    ch: 148,
+  }))
+    .mockImplementationOnce(() => ({
+      top: 203,
+      left: 240,
+      bottom: 353,
+      right: 1309,
+      width: 1069,
+      height: 150,
+      cw: 1050,
+      ch: 148,
+    }))
+    .mockImplementationOnce(() => ({
+      x: 241,
+      y: 404,
+      width: 1050,
+      height: 32,
+      top: 404,
+      right: 1291,
+      bottom: 436,
+      left: 241
+    }));
+  Object.defineProperty(HTMLDivElement.prototype, 'offsetHeight', {
+    get: () => 32
+  });
+  const wrapper = mount({
+    template: `
+    <div ref="containerRef"
+         style="height: 150px; border:1px solid #ccc; overflow:auto">
+      <div style="height:500px">
+        <div style="padding-top:200px">
+          <ux-affix :get-target="getTarget"
+                    :offset-bottom="10">
+            <button>container</button>
+          </ux-affix>
+        </div>
+      </div>
+    </div>    
+    `,
+    components: {
+      UxAffix,
+    },
+    methods: {
+      getTarget() {
+        const { $refs: { containerRef } } = this;
+        return containerRef;
+      },
+    },
+  });
+  await waitTime();
+  expect(wrapper.find('.ux-affix').element.style.bottom).toBe('425px');
 });
