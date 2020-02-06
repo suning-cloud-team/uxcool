beaff4ebb3d62371ecf4ff8328eb9d4f43043d9f
hww <309442826@qq.com>
2020-02-06 16:30:24 +0800

    test(input-number): 完善单元测试用例

--- packages/uxcool/src/components/input-number/__tests__/input-number.spec.js
+++ packages/uxcool/src/components/input-number/__tests__/input-number.spec.js
@@ -1,5 +1,27 @@
 import { mountPickerFactory, waitTime, triggerEvent } from '@suning/v-test-utils';
 import InputNumber from '../index';
+import { UxInput } from '../../input';
+import { mount } from '../../../../../v-test-utils/src';
+/*
+keydown事件
+var modifiers = {
+  enter: 13,
+  tab: 9,
+  delete: 46,
+  esc: 27,
+  space: 32,
+  up: 38,
+  down: 40,
+  left: 37,
+  right: 39,
+  end: 35,
+  home: 36,
+  backspace: 8,
+  insert: 45,
+  pageup: 33,
+  pagedown: 34
+};
+*/
 
 const mountInputNumber = mountPickerFactory(InputNumber);
 describe('input-number', () => {
@@ -57,5 +79,55 @@ describe('input-number', () => {
     });
     await waitTime();
     expect(wrapper.find('input').element.value).toBe('9900%');
+    wrapper.destroy();
+  });
+
+  it('event is correctly', async () => {
+    const changeFn = jest.fn();
+    const inputChangeFn = jest.fn();
+    const keydownFn = jest.fn();
+    const keyupFn = jest.fn();
+    const wrapper = await mountInputNumber({
+      propsData: {
+        max: 10,
+        min: 0,
+      },
+      listeners: {
+        change: changeFn,
+        keydown: keydownFn,
+        keyup: keyupFn,
+      },
+    });
+    wrapper.vm.$on('input-change', inputChangeFn);
+    wrapper.vm.focus();
+    const inputWrapper = wrapper.find('input');
+    console.log(inputWrapper.html());
+    inputWrapper.setValue(4);
+    inputWrapper.trigger('keydown.up');
+    await waitTime();
+    inputWrapper.trigger('keyup.up');
+    await waitTime();
+    inputWrapper.trigger('keydown.up');
+    await waitTime();
+    inputWrapper.trigger('keyup.up');
+    await waitTime();
+    inputWrapper.trigger('keydown.down');
+    await waitTime();
+    inputWrapper.trigger('keyup.down');
+    wrapper.vm.blur();
+    await waitTime();
+    expect(inputWrapper.element.value).toBe('5');
+    expect(changeFn).toHaveBeenCalled();
+    expect(inputChangeFn).toHaveBeenCalled();
+    expect(keydownFn).toHaveBeenCalled();
+    expect(keyupFn).toHaveBeenCalled();
+    wrapper.find('.ux-input-number-handler-up').trigger('mousedown');
+    await waitTime();
+    expect(inputWrapper.element.value).toBe('6');
+    inputWrapper.trigger('focus');
+    inputWrapper.element.value = '7';
+    inputWrapper.trigger('change');
+    await waitTime(50);
+    expect(inputWrapper.element.value).toBe('7');
   });
 });
