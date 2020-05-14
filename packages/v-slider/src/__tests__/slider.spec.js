ae58716cb4c81dacdfec907cc4213b9d7d0782ce
前端-荀啟龙(13075766) <13075766@cnsuning.com>
2020-05-14 18:07:21 +0800

    test(v-slider):修复单元测试失败问题

--- packages/v-slider/src/__tests__/slider.spec.js
+++ packages/v-slider/src/__tests__/slider.spec.js
@@ -1,278 +1,352 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { mount, mountPickerFactory, waitTime } from '@suning/v-test-utils';
+import { mountPickerFactory, triggerEvent } from '@suning/v-test-utils';
 import { VSlider, VSliderRange } from '../index';
-import {
-  getMousePosition,
-  getTouchPosition,
-  getPrecision,
-  isTargetEvent,
-  getPositionAndDragOffset,
-} from '../utils';
 
-describe('VSlider', () => {
-  it('range text click', async () => {
-    const wrapper = mount({
-      render() {
-        return (
-          <VSliderRange
-            ref="test"
-            style={{ height: '300px' }}
-            vertical
-            value={[20, 30, 50, 60]}
-            allowCross={false}
-            pushable={false}
-            marks={{ 10: '10', 40: '40' }}
-          />
-        );
-      },
-    });
-    await wrapper.vm.$nextTick();
-    wrapper.vm.$refs.test.$children[5].onClick();
-    // wrapper
-    //   .findAll('.v-slider-mark-text')
-    //   .at(1)
-    //   .trigger('click');
-    // await waitTime();
-    // expect(wrapper.find('.v-slider-handle-3').attributes('aria-valuenow')).toBe('40');
+describe('slider', () => {
+  jest.setTimeout(100000);
+  const mountSlider = mountPickerFactory(VSlider);
 
-    wrapper.destroy();
-    await waitTime();
-  });
-
-  it('focus, blur', async () => {
-    const wrapper = mount({
-      render() {
-        return (
-          <VSlider
-            id="testUt"
-            ref="handleRef"
-            value={100}
-            class="fb"
-            step={false}
-            autofocus={true}
-          />
-        );
-      },
-    });
-    await wrapper.vm.$nextTick();
-    getPositionAndDragOffset(
-      {
-        ...wrapper.vm.$refs.handleRef.$el.children[3],
-        target: wrapper.vm.$refs.handleRef.$el.children[3],
-      },
-      false,
-      false,
-      'mouse'
-    );
-    getPositionAndDragOffset(
-      {
-        ...wrapper.vm.$refs.handleRef.$el.children[3],
-        target: wrapper.vm.$refs.handleRef.$el.children[3],
-      },
-      false,
-      true,
-      'mouse'
-    );
-    getPositionAndDragOffset(
-      {
-        ...wrapper.vm.$refs.handleRef.$el.children[3],
-        target: wrapper.vm.$refs.handleRef.$el.children[3],
-      },
-      true,
-      false,
-      'mouse'
-    );
-    getPositionAndDragOffset(
-      {
-        ...wrapper.vm.$refs.handleRef.$el.children[3],
-        target: wrapper.vm.$refs.handleRef.$el.children[3],
-      },
-      true,
-      true,
-      'mouse'
-    );
-    getPositionAndDragOffset(
-      { ...new TouchEvent(100), touches: [{ clientY: 1, clientX: 2 }] },
-      false,
-      false,
-      'touch'
-    );
-    getPositionAndDragOffset(
-      { ...new TouchEvent(100), touches: [{ clientY: 1, clientX: 2 }] },
-      true,
-      false,
-      'touch'
-    );
-    isTargetEvent(new MouseEvent(100), wrapper.vm.$refs.handleRef);
-    isTargetEvent(new MouseEvent(100), null);
-    wrapper.find('.v-slider-handle').trigger('focus');
-    await waitTime();
-    expect(wrapper.find('.v-slider-handle').attributes('dragging')).toBeTruthy();
-    wrapper.find('.v-slider-handle').trigger('blur');
-    await waitTime();
-    wrapper.vm.$refs.handleRef.focus();
-    wrapper.vm.$refs.handleRef.blur();
-    wrapper.destroy();
-    await waitTime();
-  });
-
-  const mountVSlider = mountPickerFactory(VSlider);
-  it('min', async () => {
-    const wrapper = await mountVSlider({
+  it('render correctly', async () => {
+    const wrapper = await mountSlider({
       propsData: {
-        min: 30,
-        value: 0,
-      },
-    });
-    await wrapper.vm.$nextTick();
-    expect(wrapper.find('.v-slider-handle').attributes('aria-valuenow')).toBe('30');
-    wrapper.destroy();
-    await waitTime();
-  });
-  it('max', async () => {
-    const wrapper = await mountVSlider({
-      propsData: {
-        max: 80,
+        value: 50,
       },
     });
 
-    wrapper.setProps({ value: 90 });
-    await waitTime();
-    expect(wrapper.find('.v-slider-handle').attributes('aria-valuenow')).toBe('80');
-    wrapper.destroy();
-    await waitTime();
+    const handle = wrapper.find({ ref: 'sliderHandleRef' });
+    expect(handle.attributes('aria-valuenow')).toBe('50');
+    expect(handle.attributes('aria-valuemin')).toBe('0');
+    expect(handle.attributes('aria-valuemax')).toBe('100');
+
+    expect(wrapper.find('.v-slider-step').exists()).toBe(true);
+    expect(wrapper.find('.v-slider-step').isEmpty()).toBe(true);
   });
 
   it('disabled', async () => {
-    await mountVSlider({
+    const onMouseDown = jest.fn();
+    const wrapper = await mountSlider({
       propsData: {
         disabled: true,
       },
-    });
-  });
-
-  it('setSlide is func', async () => {
-    const wrapper = await mountVSlider({
-      propsData: {
-        value: 0,
+      methods: {
+        onMouseDown,
       },
     });
-    await wrapper.vm.$nextTick();
-    wrapper.vm.onStart(wrapper.vm.$event, 10);
-    wrapper.vm.onMove(new MouseEvent(100), 100);
-    wrapper.vm.onEnd(wrapper.vm.$event, 100);
-    await wrapper.vm.$nextTick();
-    // expect(wrapper.find('.v-slider-handle').attributes('aria-valuenow')).toBe('0');
-    wrapper.destroy();
-    await waitTime();
+
+    triggerEvent(wrapper, 'mousedown');
+    expect(onMouseDown).not.toBeCalled();
   });
 
-  const mountVSliderRange = mountPickerFactory(VSliderRange);
+  it('focus and blur', async () => {
+    const focusFn = jest.fn();
+    const blurFn = jest.fn();
 
-  it('range and value is array correctly', async () => {
-    const wrapper = await mountVSliderRange({
-      propsData: {
-        value: [0, 80],
-        range: true,
-        tabindex: [1, 2],
+    HTMLDivElement.prototype.getBoundingClientRect = jest.fn(() => ({
+      width: 1000,
+      height: 12,
+      x: 6,
+      y: 14,
+      left: 6,
+      top: 14,
+      right: 1000,
+      bottom: 26,
+    }));
+
+    const wrapper = await mountSlider({
+      listeners: {
+        focus: focusFn,
+        blur: blurFn,
       },
     });
+    wrapper.vm.focus();
     await wrapper.vm.$nextTick();
-    expect(wrapper.find('.v-slider-handle-1').attributes('aria-valuenow')).toBe('0');
-    expect(wrapper.find('.v-slider-handle-1').attributes('tabindex')).toBe('1');
-    expect(wrapper.find('.v-slider-handle-2').attributes('aria-valuenow')).toBe('80');
-    expect(wrapper.find('.v-slider-handle-2').attributes('tabindex')).toBe('2');
-    wrapper.destroy();
-    await waitTime();
+    expect(focusFn).toBeCalled();
+    wrapper.vm.blur();
+    await wrapper.vm.$nextTick();
+    expect(blurFn).toBeCalled();
   });
 
-  it('range and value is not defined  is correctly', async () => {
-    const wrapper = await mountVSliderRange({
-      propsData: {
-        range: true,
-        disabled: true,
+  it('drag', async () => {
+    HTMLDivElement.prototype.getBoundingClientRect = jest
+      .fn(() => ({
+        width: 1000,
+        height: 12,
+        x: 6,
+        y: 14,
+        left: 6,
+        top: 14,
+        right: 1000,
+        bottom: 26,
+      }))
+      .mockImplementationOnce(() => ({
+        width: 14,
+        height: 14,
+        x: -1,
+        y: 13,
+        left: -1,
+        top: 13,
+        right: 13,
+        bottom: 27,
+      }));
+
+    const onChange = jest.fn();
+    const wrapper = await mountSlider({
+      attachToDocument: true,
+      listeners: {
+        change: onChange,
       },
     });
-    await waitTime();
-    expect(wrapper.find('.v-slider-handle-1').attributes('aria-valuenow')).toBe('0');
-    expect(wrapper.find('.v-slider-handle-2').attributes('aria-valuenow')).toBe('0');
-    wrapper.destroy();
-    await waitTime();
-  });
+    const handler = wrapper.find({ ref: 'sliderHandleRef' });
 
-  it('setSlideRange is func', async () => {
-    const wrapper = await mountVSliderRange({
-      propsData: {
-        value: [0, 100],
-        allowCross: false,
-        marks: { 10: '10', 40: '40' },
-      },
+    await triggerEvent(handler, 'mousedown', {
+      clientX: 4,
+      clientY: 17,
+      button: 2,
     });
-    await wrapper.vm.$nextTick();
-    wrapper.vm.onStart(wrapper.vm.$event, 100);
-    await wrapper.vm.$nextTick();
-    wrapper.vm.onMove(new MouseEvent(100), 100);
-    await wrapper.vm.$nextTick();
-    wrapper.vm.onEnd(wrapper.vm.$event, 100);
-    await wrapper.vm.$nextTick();
-    expect(wrapper.find('.v-slider-handle-2').attributes('aria-valuenow')).toBe('100');
+
+    expect(onChange).not.toBeCalled();
+    expect(wrapper.vm.innerValue).toBe(0);
+
+    await triggerEvent(handler, 'mousedown', {
+      clientX: 4,
+      clientY: 17,
+      button: 0,
+    });
+
+    expect(onChange).not.toBeCalled();
+
+    await triggerEvent(wrapper, 'mousemove', {
+      clientX: 1500,
+      clientY: 28,
+    });
+
+    expect(onChange).toBeCalled();
+    expect(wrapper.vm.innerValue).toBe(100);
+
     wrapper.destroy();
-    await waitTime();
   });
 
-  it('setSlideRange is addPointTracks', async () => {
-    const wrapper = await mountVSliderRange({
+  it('step', async () => {
+    const wrapper = await mountSlider({
       propsData: {
-        value: [0, 100],
-        marks: { 10: '10', 40: '40' },
         step: 2,
         min: 0,
-        max: 100,
+        max: 10,
         dots: true,
+      },
+    });
+
+    expect(wrapper.findAll('.v-slider-dot').length).toBe(6);
+  });
+
+  it('marks', async () => {
+    const wrapper = await mountSlider({
+      propsData: {
+        step: 2,
+        min: 0,
+        max: 10,
+        marks: {
+          0: '0℃',
+          2: '2℃',
+          10: {
+            style: {
+              color: 'green',
+            },
+            label: '10℃',
+          },
+        },
+        attachToDocument: true,
+      },
+    });
+    expect(wrapper.findAll('.v-slider-mark-text').length).toBe(3);
+
+    await triggerEvent(wrapper.findAll('.v-slider-mark-text').at(2), 'mousedown', {
+      clientX: 1000,
+      clientY: 39,
+      button: 0,
+    });
+    expect(wrapper.vm.innerValue).toBe(10);
+    wrapper.destroy();
+  });
+
+  it('vertical', async () => {
+    HTMLDivElement.prototype.getBoundingClientRect = jest.fn(() => ({
+      width: 14,
+      height: 500,
+      x: 0,
+      y: 0,
+      left: 0,
+      top: 0,
+      right: 14,
+      bottom: 500,
+    }));
+
+    const onChange = jest.fn();
+    const wrapper = await mountSlider({
+      propsData: {
+        step: 2,
+        min: 0,
+        max: 10,
         vertical: true,
       },
-    });
-    await wrapper.vm.$nextTick();
-    getPrecision(2.6);
-    wrapper.vm.onChange(100);
-    expect(wrapper.find('.v-slider-handle-2').attributes('aria-valuenow')).toBe('100');
-    wrapper.destroy();
-    await waitTime();
-  });
-
-  it('setSlideRange is moveTo', async () => {
-    const wrapper = await mountVSliderRange({
-      propsData: {
-        value: [0, 100],
-        // marks: { 10: '10', 40: '40' },
+      listeners: {
+        change: onChange,
       },
     });
-    wrapper.vm.ensureMovePoint(90);
-    wrapper.vm.addPointTracks();
-    wrapper.vm.moveTo(90);
-    await wrapper.vm.$nextTick();
-    expect(wrapper.find('.v-slider-handle-1').attributes('aria-valuenow')).toBe('90');
-    wrapper.destroy();
-    await waitTime();
+
+    await triggerEvent(wrapper, 'mousedown', {
+      clientX: 5,
+      clientY: 250,
+      button: 0,
+    });
+
+    expect(onChange).toBeCalled();
   });
 
-  it('setSlideRange is ensureValueNotConflict', async () => {
-    const wrapper = await mountVSliderRange({
+  it('included', async () => {
+    const wrapper = await mountSlider({
       propsData: {
-        value: [0, 100],
-        allowCross: false,
+        value: 50,
+      },
+    });
+
+    expect(wrapper.find('.v-slider-track').exists()).toBe(true);
+
+    wrapper.setProps({
+      included: false,
+    });
+    await wrapper.vm.$nextTick();
+    expect(wrapper.find('.v-slider-track').exists()).toBe(false);
+  });
+
+  it('watch', async () => {
+    const onChange = jest.fn();
+    const wrapper = await mountSlider({
+      propsData: {
+        value: 20,
         min: 0,
         max: 100,
       },
+      listeners: {
+        change: onChange,
+      },
     });
-    getMousePosition(new MouseEvent(100), false);
-    getMousePosition(new MouseEvent(100), true);
-    wrapper.vm.ensureValueNotConflict(0, 0);
+
+    expect(wrapper.vm.innerValue).toBe(20);
+
+    wrapper.setProps({
+      min: 40,
+    });
     await wrapper.vm.$nextTick();
-    expect(wrapper.find('.v-slider-handle-1').attributes('aria-valuenow')).toBe('0');
-    getTouchPosition({ ...new TouchEvent(100), touches: [{ clientY: 1, clientX: 2 }] }, true);
-    getTouchPosition({ ...new TouchEvent(100), touches: [{ clientY: 1, clientX: 2 }] }, false);
+    expect(onChange).lastCalledWith(40);
+    expect(wrapper.vm.innerValue).toBe(40);
+  });
+});
+
+describe('sliderRange', () => {
+  const mountSliderRange = mountPickerFactory(VSliderRange);
+
+  it('render correctly', async () => {
+    const onChange = jest.fn();
+    const wrapper = await mountSliderRange({
+      propsData: {
+        value: [],
+      },
+      listeners: {
+        change: onChange,
+      },
+    });
+
+    expect(wrapper.findAll({ ref: 'sliderHandleRef' }).length).toBe(2);
+    expect(wrapper.findAll('.v-slider-track').length).toBe(1);
+    expect(wrapper.find('.v-slider-dot').exists()).toBe(false);
+    expect(wrapper.find('.v-slider-mark-text').exists()).toBe(false);
+    expect(wrapper.vm.innerValue).toEqual([0, 0]);
+
+    wrapper.setProps({
+      value: [20, 30],
+    });
+    await wrapper.vm.$nextTick();
+    expect(wrapper.vm.innerValue).toEqual([20, 30]);
+    expect(onChange).lastCalledWith([20, 30]);
+  });
+
+  it('drag', async () => {
+    HTMLDivElement.prototype.getBoundingClientRect = jest.fn(() => ({
+      width: 1000,
+      height: 14,
+      x: 0,
+      y: 0,
+      left: 0,
+      top: 0,
+      right: 1000,
+      bottom: 14,
+    }));
+
+    const onChange = jest.fn();
+    const wrapper = await mountSliderRange({
+      propsData: {
+        value: [20, 30],
+      },
+      listeners: {
+        change: onChange,
+      },
+      attachToDocument: true,
+    });
+
+    await triggerEvent(wrapper, 'mousedown', {
+      clientX: 240,
+      clientY: 14,
+      button: 0,
+    });
+
+    expect(onChange).toBeCalled();
+
+    await triggerEvent(wrapper, 'mousemove', {
+      clientX: 1000,
+      clientY: 14,
+    });
+
+    expect(wrapper.vm.innerValue).toEqual([30, 100]);
+    await triggerEvent(wrapper, 'mouseup');
+    expect(wrapper.emitted()['after-change']).toBeTruthy();
+
     wrapper.destroy();
-    await waitTime();
+  });
+
+  it('not allowCross', async () => {
+    HTMLDivElement.prototype.getBoundingClientRect = jest.fn(() => ({
+      width: 1000,
+      height: 14,
+      x: 0,
+      y: 0,
+      left: 0,
+      top: 0,
+      right: 1000,
+      bottom: 14,
+    }));
+
+    const wrapper = await mountSliderRange({
+      propsData: {
+        value: [20, 30],
+        allowCross: false,
+      },
+      attachToDocument: true,
+    });
+
+    await triggerEvent(wrapper, 'mousedown', {
+      clientX: 740,
+      clientY: 14,
+      button: 0,
+    });
+
+    await triggerEvent(wrapper, 'mousemove', {
+      clientX: 0,
+      clientY: 14,
+    });
+
+    expect(wrapper.vm.innerValue).toEqual([20, 20]);
+
+    wrapper.destroy();
   });
 });
